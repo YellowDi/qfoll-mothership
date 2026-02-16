384c6cd115540f0d4a7fc28a81ff557e083d3de7
Asuka Langley <yellowdi@me.com>
2026-02-16 17:20:18 +0800

    build: add image optimization plugins for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { imagetools } from "vite-imagetools";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    imagetools({
+      removeMetadata: true,
+    }),
+    ViteImageOptimizer({
+      includePublic: true,
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { quality: 80 },
+    }),
+  ],
+});
